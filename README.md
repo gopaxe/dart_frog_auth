e29a193fbea43f9641aad6c008fc049cdfdd9301
ofceab <flutternow1963@gmail.com>
2023-12-29 11:06:07 +0000

    Updated dart_frog url

---
+++ README.md
@@ -0,0 +1,36 @@
+[![Dart Frog Logo][logo_white]][dart_frog_link_dark]
+[![Dart Frog Logo][logo_black]][dart_frog_link_light]
+
+[![ci][ci_badge]][ci_link]
+[![coverage][coverage_badge]][ci_link]
+[![pub package][pub_badge]][pub_link]
+[![style: very good analysis][very_good_analysis_badge]][very_good_analysis_link]
+[![License: MIT][license_badge]][license_link]
+
+Header Authorization support for [Dart Frog][dart_frog_link].
+
+Developed with 💙 by [Very Good Ventures][very_good_ventures_link] 🦄
+
+## About
+
+A package that provides the foundation for authentication for a Dart Frog backend server.
+
+Learn more about it on the [official docs][docs_link].
+
+[ci_badge]: https://github.com/VeryGoodOpenSource/dart_frog/actions/workflows/dart_frog_auth.yaml/badge.svg?branch=main
+[ci_link]: https://github.com/VeryGoodOpenSource/dart_frog/actions/workflows/dart_frog_auth.yaml
+[coverage_badge]: https://raw.githubusercontent.com/VeryGoodOpenSource/dart_frog/main/packages/dart_frog_web_socket/coverage_badge.svg
+[dart_frog_link]: https://github.com/verygoodopensource/dart_frog
+[dart_frog_link_dark]: https://github.com/verygoodopensource/dart_frog#gh-dark-mode-only
+[dart_frog_link_light]: https://github.com/verygoodopensource/dart_frog#gh-light-mode-only
+[license_badge]: https://img.shields.io/badge/license-MIT-blue.svg
+[license_link]: https://opensource.org/licenses/MIT
+[logo_black]: https://raw.githubusercontent.com/VeryGoodOpenSource/dart_frog/main/assets/dart_frog_logo_black.png#gh-light-mode-only
+[logo_white]: https://raw.githubusercontent.com/VeryGoodOpenSource/dart_frog/main/assets/dart_frog_logo_white.png#gh-dark-mode-only
+[pub_badge]: https://img.shields.io/pub/v/dart_frog_auth.svg
+[pub_link]: https://pub.dartlang.org/packages/dart_frog_auth
+[very_good_analysis_badge]: https://img.shields.io/badge/style-very_good_analysis-B22C89.svg
+[very_good_analysis_link]: https://pub.dev/packages/very_good_analysis
+[very_good_ventures_link]: https://verygood.ventures
+[general_http]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Authentication#the_general_http_authentication_framework
+[docs_link]: https://dartfrog.vgv.dev/docs/advanced/authentication
---
+++ lib/src/dart_frog_auth.dart
@@ -0,0 +1,156 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+
+extension on Map<String, String> {
+  String? authorization(String type) {
+    final value = this['Authorization']?.split(' ');
+
+    if (value != null && value.length == 2 && value.first == type) {
+      return value.last;
+    }
+
+    return null;
+  }
+
+  String? bearer() => authorization('Bearer');
+  String? basic() => authorization('Basic');
+}
+
+/// Function definition for the predicate function used by Dart Frog Auth
+/// middleware to determine if the request should be authenticated or not.
+typedef Applies = Future<bool> Function(RequestContext);
+
+Future<bool> _defaultApplies(RequestContext context) async => true;
+
+/// Authentication that uses the `Authorization` header with the `Basic` scheme.
+///
+/// `Basic` scheme expects the header to be in the format:
+/// ```
+/// Authorization: Basic <token>
+/// ```
+///
+/// Token should be a base64 encoded string of the format:
+/// ```
+/// <username>:<password>
+/// ```
+///
+/// In order to use this middleware, you must provide a function that will
+/// return a user object from the username, password and request context.
+///
+/// If the given function returns null for the given username and password,
+/// the middleware will return a `401 Unauthorized` response.
+///
+/// By default, this middleware will apply to all routes. You can change this
+/// behavior by providing a function that returns a boolean value based on the
+/// [RequestContext]. If the function returns false, the middleware will not
+/// apply to the route and the call will have authentication validation.
+Middleware basicAuthentication<T extends Object>({
+  @Deprecated(
+    'Deprecated in favor of authenticator. '
+    'This will be removed in future versions',
+  )
+  Future<T?> Function(
+    String username,
+    String password,
+  )? userFromCredentials,
+  Future<T?> Function(
+    RequestContext context,
+    String username,
+    String password,
+  )? authenticator,
+  Applies applies = _defaultApplies,
+}) {
+  assert(
+    userFromCredentials != null || authenticator != null,
+    'You must provide either a userFromCredentials or a '
+    'authenticator function',
+  );
+  return (handler) => (context) async {
+        if (!await applies(context)) {
+          return handler(context);
+        }
+
+        Future<T?> call(String username, String password) async {
+          if (userFromCredentials != null) {
+            return userFromCredentials(username, password);
+          } else {
+            return authenticator!(context, username, password);
+          }
+        }
+
+        final authorization = context.request.headers.basic();
+        if (authorization != null) {
+          final [username, password] =
+              String.fromCharCodes(base64Decode(authorization)).split(':');
+
+          final user = await call(username, password);
+          if (user != null) {
+            return handler(context.provide(() => user));
+          }
+        }
+
+        return Response(statusCode: HttpStatus.unauthorized);
+      };
+}
+
+/// Authentication that uses the `Authorization` header with the `Bearer`
+/// scheme.
+///
+/// `Bearer` scheme expects the header to be in the format:
+/// ```
+/// Authorization: Bearer <token>
+/// ```
+///
+/// The token format is up to the user. Usually it will be an encrypted token.
+///
+/// In order to use this middleware, you must provide a function that will
+/// return a user object from the received token and request context.
+///
+/// If the given function returns null for the given username and password,
+/// the middleware will return a `401 Unauthorized` response.
+///
+/// By default, this middleware will apply to all routes. You can change this
+/// behavior by providing a function that returns a boolean value based on the
+/// [RequestContext]. If the function returns false, the middleware will not
+/// apply to the route and the call will have no authentication validation.
+Middleware bearerAuthentication<T extends Object>({
+  @Deprecated(
+    'Deprecated in favor of authenticator. '
+    'This will be removed in future versions',
+  )
+  Future<T?> Function(String token)? userFromToken,
+  Future<T?> Function(RequestContext context, String token)? authenticator,
+  Applies applies = _defaultApplies,
+}) {
+  assert(
+    userFromToken != null || authenticator != null,
+    'You must provide either a userFromToken or a '
+    'authenticator function',
+  );
+
+  return (handler) => (context) async {
+        if (!await applies(context)) {
+          return handler(context);
+        }
+
+        Future<T?> call(String token) async {
+          if (userFromToken != null) {
+            return userFromToken(token);
+          } else {
+            return authenticator!(context, token);
+          }
+        }
+
+        final authorization = context.request.headers.bearer();
+        if (authorization != null) {
+          final user = await call(authorization);
+          if (user != null) {
+            return handler(context.provide(() => user));
+          }
+        }
+
+        return Response(statusCode: HttpStatus.unauthorized);
+      };
+}
